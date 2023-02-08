b693616af0e4327710ec4b8369444bb1c2d4f59f
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-02-08 22:48:22 +0700

    secondly commit

---
+++ routes/authRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  createUser,
+  loginUserCtrl,
+  getAllUser,
+  getaUser,
+  deleteaUser,
+  updatedUser,
+  blockUser,
+  unblockUser,
+} = require("../controllers/userCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post("/register", createUser);
+router.post("/login", loginUserCtrl);
+router.get("/all-users", getAllUser);
+router.get("/:id", authMiddleware, isAdmin, getaUser);
+router.delete("/:id", deleteaUser);
+router.put("/edit-user", authMiddleware, updatedUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
+
+module.exports = router;
