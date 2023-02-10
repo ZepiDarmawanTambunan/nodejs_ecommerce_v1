91ed4c8979f9162c919acee2edb0277fde302c93
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-02-10 19:09:51 +0700

    fively - filter sort paginate limit

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -51,8 +51,51 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getAllProduct = asyncHandler(async (req, res) => {
   try {
-    const getallProducts = await Product.find();
-    res.json(getallProducts);
+    // query yg ada page, sort, limit dan fields akan dihapus dari url tapi data tetap ada di req.query
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    // localhost:5000/api/product?page=2&limit=2
+    // console.log(req.query); { page: '2', limit: '2' }
+    // console.log(queryObj); {}
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // localhost:5000/api/product?price[gte]=100
+    // console.log(queryObj); => {price: {gte: '100'}}
+    // console.log(queryStr); => {price: {'$gte': '100'}}
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    // SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      // query = query.sort("createdAt"); // SORT ASC [1-10] [A-Z]
+      query = query.sort("-createdAt"); // SORT DESC [10-1] [Z-A]
+    }
+
+    // LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // PAGINATION
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error("This page does not exists");
+    }
+
+    const product = await query;
+    res.json(product);
   } catch (error) {
     throw new Error(error);
   }
